6213d98157ad349bebd63322d189cdbd81e06f97
31010-52-05 <maxime.ayoub@gmail.com>
2018-10-18 10:10:03 +0200

    debut

--- ctrl/ctrlGeneral.php
+++ ctrl/ctrlGeneral.php
@@ -5,10 +5,33 @@
         private $user;
 
         function __construct(){
-            $this->vue=new viewGenerale();
+            // $this->vue=new viewGeneral();
             $this->model=new modelGeneral();
         }
 
-    
+        public function getConnection(){
+            $this->view->afficherConnection();
+        }
+
+        public function getAuthentification(){
+            $this->view->afficherConnection();
+        }
+
+        public function verifierAuthentification(){
+            $a['identifiant']="maxime@gmail.com";
+            // $a['identifiant']="max";
+            $a['motdepasse']="toulouse31";
+            $this->user=new Compte($a);
+            var_dump($this->user);
+            // $this->user=new Compte($_POST);
+            $verifAuthentification=$this->model->enregistrerFormulaire($this->user);
+            var_dump($verifAuthentification);
+            if ($verifAuthentification){
+
+            }else{
+                
+            }
+        }
+
     }
 ?>
--- model/modelGeneral.php
+++ model/modelGeneral.php
@@ -6,6 +6,35 @@
             $this->dao=new viewGenerale();
         }
 
+        public function enregistrerFormulaire($obj){
+            if ($obj->getTypetape()!=""){
+                if ($obj->getTypetape()=="email"){
+                    $requete = "SELECT `mot_de_passe` `admin` FROM `User` WHERE `email`='".$obj->getIdentifiant()."'";
+                }else {
+                    $requete = "SELECT `mot_de_passe` `admin` FROM `User` WHERE `pseudo`='".$obj->getIdentifiant()."'";
+                }
+                // $requete="SELECT `mot_de_passe` FROM `User` WHERE `email`='maxime@gmail.com'";
+                if($result = $this->dao->bddQuery($requete)){
+                    // traiter le retour
+                    $compte = array();
+                    foreach($result as $o){
+                        $compte[] = $o;
+                    }
+                }
+                else{
+                    // gerer l'erreur
+                var_dump($result);
+                    return false;
+                }
+                var_dump($result);
+                if ($result[0]['mot_de_passe']==$obj->getPassword()){
+                    // if ($result[0]['mot_de_passe']=="toulouse31"){
+                    return true;
+                }else{
+                    return false;
+                }
+            }
+        }
     
     }
 ?>
