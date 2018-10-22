8f14c809284bad4c4f37d600df0cdbea291c4409
DCcarvalho <decarvalho.karl@gmail.com>
2018-10-22 13:31:18 +0200

    navbar pop up connect

--- templates/partials/identification.php
+++ templates/partials/identification.php
@@ -1,3 +1,15 @@
+<style>
+        input[type=text], select {
+            width: 100%;
+            padding: 12px 20px;
+            margin: 8px 0;
+            display: inline-block;
+            border: 1px solid #ccc;
+            border-radius: 4px;
+            box-sizing: border-box;
+        }
+        </style>
+
 <form class="form-horizontal ;" style="padding:10%;" action="index.php?c=ctrlGeneral&m=verifierAuthentification" method="POST">
         <div class="form-group">
           <h1 style="text-align: center;">Connexion</h1>
@@ -27,5 +39,58 @@
           <div class="col-sm-offset-2 col-sm-10">
             <button type="submit" class="btn btn-default">Se connecter</button>
           </div>
+          <p data-toggle="modal" data-target="#myModal1" style="text-align: center;">Identifiant ou mot de passe oublié</p>
         </div>
       </form>
+      
+      
+      
+      <div class="modal fade" id="myModal1" role="dialog">
+            <div class="modal-dialog">
+            
+              <!-- Modal content-->
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                  <h4 class="modal-title">Modifier le mot de passe ou identifiant</h4>
+                </div>
+                
+                <div class="modal-body">
+                  <p>Entrez l'adresse courriel associée à votre compte pour recevoir les instructions de réinitialisation de votre mot de passe.</p>
+                    <input type="text" id="fname" name="firstname" placeholder="Email">
+                </div>
+                
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-default" data-dismiss="modal">Annuler</button>
+                  <button type="button" class="btn btn-default"  data-toggle="modal" data-target="#confirmation">valider</button>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+
+               <!-- Modal confirmation de demande -->
+               <div class="modal fade" id="confirmation" role="dialog">
+                    <div class="modal-dialog">
+                    
+                        <!-- Modal content-->
+                        <div class="modal-content">
+                        <div class="modal-header">
+                          
+                          <button type="button" class="close" data-dismiss="modal">&times;</button>
+                          <h4 class="modal-title">Confirmation de demande</h4>
+                        </div>
+                        
+                        <div class="modal-body">
+                                <p>Un email vous a bien été envoyé pour repondre à votre demande.</p>
+                                  
+                              </div>
+                        
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-default" data-dismiss="modal">Annuler</button>
+                          <button type="button" class="btn btn-default" data-dismiss="modal">valider</button>
+                        </div>
+                      </div>
+                      
+                    </div>
+                  </div>  
--- templates/partials/navBarConnect.php
+++ templates/partials/navBarConnect.php
@@ -1,10 +1,5 @@
-
-
-
-
-
- <div id="nav">
-        <div class="navbar navbar-inverse navbar-fixed-top" data-spy="affix" data-offset-top="100">
+<div id="nav">
+        <div id= "navresp" class="navbar navbar-inverse navbar-fixed-top" data-spy="affix" data-offset-top="100">
             <div class="container">
               <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -13,12 +8,12 @@
                   <span class="icon-bar"></span>
                   <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" id ="logo" style='padding: 0 0 0 4%;' href="#"><img  class="img-responsive logo" src="images/logo.png" alt=""></a>
+                <a class="navbar-brand" style='padding: 0px 0px;' href="#"><img id ="logo" class="img-responsive logo" src="images/logo.png" alt=""></a>
               </div>
               <div id="navbar" class="collapse navbar-collapse">
                   <ul class="nav navbar-nav navbar-left" ><li id ="home" ><a href="">Home</a>
                   </li> </ul>
-                
+               
                 <ul class="nav navbar-nav navbar-right" >
                  
                   <li ><a id = "icon" class="lien" href="#" style="padding: 0px;padding-top: 4px;padding-right: 10px;"  ><img src="images\androidicon.png "></a>
@@ -26,11 +21,24 @@
                    
                   <li><a id = "icon1" class="lien" href="#" style="padding: 0px;padding-top: 4px;padding-right: 10px; " ><img src="images\appleicon.png "></a>
                   </li>
-                  <li><a  id = "inscrire" href="ctrlGeneral/getForm">S'inscrire</a></li>
+                  <li><a  id = "inscrire" href="#">S'inscrire</a></li>
                     
-                  <li ><a id = "connecter" href="ctrlGeneral/getAuthentification" >Se connecter</a></li>
-                      
+                  <li ><a id = "connecter" href="#" >Se connecter</a></li>
+                 
+                    <li class="dropdown">
+                      <a id="admin" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Admin 
+                          <span ><img src="images\avataricon.jpg "></span>
+                        </a>
+                      <ul  class="dropdown-menu" >
+                        <li><a href="#">Profil</a></li>
+                        <li><a href="#">Mon compte</a></li>
+                        <li><a href="#">Paramètres</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Se déconnecter</a></li>
+                      </ul>
+                    </li>  
                 </ul>
+                
               </div>
              
             </div>
