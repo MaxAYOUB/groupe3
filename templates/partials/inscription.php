50c1495153f4fa42097251ab1421cf296fe92c17
DCcarvalho <decarvalho.karl@gmail.com>
2018-10-23 09:26:02 +0200

    mise a jour pop

--- templates/partials/inscription.php
+++ templates/partials/inscription.php
@@ -190,3 +190,59 @@
 
         </form>
 </div>
+
+
+
+          <!--pop up  Modal Succes -->
+          <div class="modal fade" id="confirmation" role="dialog">
+                    <div class="modal-dialog">
+                    
+                        <!-- Modal content-->
+                        <div class="modal-content">
+                        <div class="modal-header">
+                          
+                          <button type="button" class="close" data-dismiss="modal">&times;</button>
+                          <h4 class="modal-title">Succès</h4>
+                        </div>
+                        
+                        <div class="modal-body">
+                                <p>Votre compte a été bien crée.</p>
+                                  
+                              </div>
+                        
+                        <div class="modal-footer">
+                         
+                          <button type="button" class="btn btn-default" data-dismiss="modal">OK</button>
+                        </div>
+                      </div>
+                      
+                    </div>
+                  </div>  
+
+
+                  
+          <!--pop up Modal Erreur -->
+          <div class="modal fade" id="confirmation" role="dialog">
+                    <div class="modal-dialog">
+                    
+                        <!-- Modal content-->
+                        <div class="modal-content">
+                        <div class="modal-header">
+                          
+                          <button type="button" class="close" data-dismiss="modal">&times;</button>
+                          <h4 class="modal-title">Erreur</h4>
+                        </div>
+                        
+                        <div class="modal-body">
+                                <p>Raison de l'erreur.</p>
+                                  
+                              </div>
+                        
+                        <div class="modal-footer">
+                         
+                          <button type="button" class="btn btn-default" data-dismiss="modal">OK</button>
+                        </div>
+                      </div>
+                      
+                    </div>
+                  </div>
