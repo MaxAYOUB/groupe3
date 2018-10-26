20aee802ef37822bca809d212c6002f77bcb5c7e
MaxAYOUB <44062660+MaxAYOUB@users.noreply.github.com>
2018-10-26 10:42:20 +0200

    Update DAO_mysql.php

--- system/DAO_mysql.php
+++ system/DAO_mysql.php
@@ -56,20 +56,20 @@ class DAO_mysql
     }
 
     private function configBdd(){
-        return $bdd = array(
-           'host'  =>  "localhost",
-           'user'  =>  "root",
-           'pass'  =>    "",
-           'database'=>    "application_gps",
-           'charset'   =>  "utf8"
-        );
         // return $bdd = array(
-        // 'host'  =>  "localhost",
-        // 'user'  =>  "pvi-s01",
-        // 'pass'  =>    "philippe31",
-        // 'database'=>    "appli_GPS",
-        // 'charset'   =>  "utf8"
+          //  'host'  =>  "localhost",
+          //  'user'  =>  "root",
+          //  'pass'  =>    "",
+          //  'database'=>    "application_gps",
+           //'charset'   =>  "utf8"
         // );
+        return $bdd = array(
+        'host'  =>  "localhost",
+        'user'  =>  "pvi-s01",
+        'pass'  =>    "philippe31",
+        'database'=>    "appli_GPS",
+         'charset'   =>  "utf8"
+        );
     }
 }
 
