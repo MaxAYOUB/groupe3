d4bf6ddc6e3c157a341c4c4c93b61672aae0ba1d
MaxAYOUB <44062660+MaxAYOUB@users.noreply.github.com>
2018-10-26 10:42:55 +0200

    Update head.php

--- templates/partials/head.php
+++ templates/partials/head.php
@@ -1,8 +1,8 @@
 <meta charset="UTF-8">
 <title>TITRE DE L'APP</title>
 <link rel="shortcut icon" type="image/png" href=""/> <!--changer le favicon-->
-<base href="http://localhost/groupe3.1/"/>
-    <!-- <base href="http://cdi.afpa-balma.fr"/> -->
+    <!-- <base href="http://localhost/groupe3.1/"/> -->
+<base href="http://cdi.afpa-balma.fr"/>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> <!--bootstrap 3-->
 <link rel="stylesheet" type="text/css" href="css/nav.css"/>
 <link rel="stylesheet" type="text/css" href="css/cssAccueil.css"/>
