58e59cb8af92844471bda25498e017c0240f31d2
31010-52-05 <maxime.ayoub@gmail.com>
2018-10-31 13:53:59 +0100

    ajout utilisateur admin

--- templates/partials/accueilNew.php
+++ templates/partials/accueilNew.php
@@ -1,5 +1,5 @@
 <!--Modifié par Babacar et Jade-->
-
+<!--Modifié par Betty-->
 <div class="container">
 
     <div id="divTel"
@@ -8,18 +8,18 @@
     </div>
 
     <div id="divPresentation" class="container-fluid col-xs-11 col-sm-11 col-md-7 col-lg-7">
-        <h1 id="h1Presentation">Présentation globale de l'APP</h1>
-        <p id="pPresentation">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-            do eiusmod tempor
-            incididunt ut labore et dolore
-            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-            commodo
-            consequat.</p>
-        <img id="imgPresentation" class="img-responsive" src="images/map2.png">
+        <h1 id="h1Presentation">Qu'est ce que TRACK ME ?</h1>
+      
+        <p id="pPresentation"> <span id="description">TRACK ME </span>permet une géolocalisation en temps réel avec la possibilité 
+            de visualiser votre itinéraire, de discuter avec les autres utilisateurs grâce au Chat,
+             de gérer vos Avatars et de gérer vos points de rendez-vous.
+
+</p>
+        <img id="imgPresentation" class="img-responsive" src="">
         <hr>
         <a id="aGooglePlay"
            class="btn btn-default col-xs-4 col-xs-offset-1 col-sm-3 col-sm-offset-2 col-md-3 col-md-offset-2 col-lg-3 col-lg-offset-2"
-           href="ctrlGeneral/telechargerAPK" target="_blank" role="button">Google Play</a>
+           href="https://play.google.com/store/apps?hl=fr" target="_blank" role="button">Google Play</a>
         <a id="aNox"
            class="btn btn-default col-xs-4 col-xs-offset-2 col-sm-3 col-sm-offset-2 col-md-3 col-md-offset-2 col-lg-3 col-lg-offset-2"
            href="https://www.bignox.com/" target="_blank" role="button">Download Nox</a>
@@ -28,44 +28,38 @@
     <div id="divFonctionnalite1" class="container-fluid col-xs-11 col-sm-11 col-md-11 col-lg-11">
         <h1 id="h1Fonctionnalite1">Localisation</h1>
         <p id="pFonctionnalite1" class="col-xs-12 col-sm-12 col-md-12 col-lg-4">
-            <span id="description">Avec MyGeoApp vous serez en mesure de vous géolocaliser.</span><br/><br/> Localiser les autres users en temps réel mais
+            <span id="description">Avec TRACK ME vous serez en mesure de vous géolocaliser.</span><br/><br/> Localiser les autres utilisateurs en temps réel mais
             également de visualiser vos rendez-vous et leurs infos en un coup d’œil. <br/><br/>
             Vous pouvez tracer la route vers votre nouveaux rendez-vous et avoir son itinéraire en un clic !
             Pratique non ?
         </p>
-        <img id="imgFonctionnalite1" class="col-xs-12 col-sm-12 col-md-12 col-lg-8" src="images/map.png">
+        <img id="imgFonctionnalite1" class="col-xs-12 col-sm-12 col-md-12 col-lg-8" src="images/localise.png">
     </div>
 
     <div id="divFonctionnalite2" class="container-fluid col-xs-11 col-sm-11 col-md-11 col-lg-11">
-        <h1 id="h1Fonctionnalite2">Fonctionnalité 2 - Chat</h1>
-        <p id="pFonctionnalite2" class="col-xs-12 col-sm-12 col-md-12 col-lg-4">Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-            commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur.</p>
-        <img id="imgFonctionnalite2" class="col-xs-12 col-sm-12 col-md-12 col-lg-8" src="images/map.png">
+        <h1 id="h1Fonctionnalite2">Chat</h1>
+        <p id="pFonctionnalite2" class="col-xs-12 col-sm-12 col-md-12 col-lg-4"><span id="description">TRACK ME </span>intègre tout un module de discussions à la fois publiques et privées.<br/>
+Il vous est possible de discuter en direct avec tous les utilisateurs de l'application qui sont connectés, tous en même temps !<br/>
+Si vous désirez écrire à une personne en particulier, il vous est toujours possible de le faire à travers la messagerie privée.<br/>
+Personne n'aura accès aux messages échangés en privés.
+</p>
+        <img id="imgFonctionnalite2" class="col-xs-12 col-sm-12 col-md-12 col-lg-8" src="">
     </div>
 
     <div id="divFonctionnalite3" class="container-fluid col-xs-11 col-sm-11 col-md-11 col-lg-11">
-        <h1 id="h1Fonctionnalite3">Fonctionnalité 3 - Avatar</h1>
-        <p id="pFonctionnalite3" class="col-xs-12 col-sm-12 col-md-12 col-lg-4">Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-            commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur.</p>
-        <img id="imgFonctionnalite3" class="col-xs-12 col-sm-12 col-md-12 col-lg-8" src="images/map.png">
+        <h1 id="h1Fonctionnalite3">Avatars</h1>
+        <p id="pFonctionnalite3" class="col-xs-12 col-sm-12 col-md-12 col-lg-4">
+Vous avez accès à une galerie d’Avatars spécial Halloween pour personnaliser  votre profil.<br/><br/> Vous avez la possibilité de modifier votre Avatar par la suite dans vos paramètres.
+</p>
+        <img id="imgFonctionnalite3" class="col-xs-12 col-sm-12 col-md-12 col-lg-8" src="images/AvatarsAccueil.png">
     </div>
 
     <div id="divFonctionnalite4" class="container-fluid col-xs-11 col-sm-11 col-md-11 col-lg-11">
-        <h1 id="h1Fonctionnalite4">Fonctionnalité 4 - Points de RDV</h1>
-        <p id="pFonctionnalite4" class="col-xs-12 col-sm-12 col-md-12 col-lg-4">Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-            magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-            commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur.</p>
-        <img id="imgFonctionnalite4" class="col-xs-12 col-sm-12 col-md-12 col-lg-8" src="images/map.png">
+        <h1 id="h1Fonctionnalite4">Points de RDV</h1>
+        <p id="pFonctionnalite4" class="col-xs-12 col-sm-12 col-md-12 col-lg-4">Vous pouvez également afficher une liste complète de tous les évènements qui se déroulent en ce moment. 
+        <br/><br/>  Vous pourrez en créer vous-même,
+les modifier et les supprimer.<br/><br/>  Il seront automatiquement supprimés dés lors que la date de fin sera atteinte ! Il apparaitront directement
+sur la carte.</p>
+        <img id="imgFonctionnalite4" class="col-xs-12 col-sm-12 col-md-12 col-lg-8" src="images/PointDeRDV.png">
     </div>
 </div>
